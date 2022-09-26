05a2c6aceaf18061db69a8636ad985d2e96bdf44
CloudDear <200710784@students.uajy.ac.id>
2022-09-26 08:25:17 +0700

    Commit Tugas (1)

---
+++ process/editSeriesProcess(CRUD).php
@@ -0,0 +1,44 @@
+<?php
+    session_start();
+    if(isset($_POST['editSeries'])){
+
+        // untuk mengoneksikan dengan database dengan memanggil file db.php
+        include('../db.php');
+
+        // tampung nilai yang ada di from ke variabel
+        // sesuaikan variabel name yang ada di registerPage.php disetiap input
+        $id = $_POST['id'];
+        $name = $_POST['name'];
+        $genre = "";
+        foreach ($_POST['genre'] as $x) {
+            $genre= $genre.$x.", ";
+        }
+        $realease = $_POST['realease'];
+        $episode = $_POST['episode'];
+        $season = $_POST['season'];
+        $synopsis = $_POST['synopsis'];
+
+        $query = mysqli_query($con," UPDATE series SET name='$name', genre='$genre', realease='$realease', episode='$episode',  season='$season', synopsis='$synopsis' WHERE id='$id'") 
+        or die(mysqli_error($con)); 
+
+        if($query){
+            echo
+                '<script>
+                alert("Edit Series Success"); 
+                window.location = "../page/editSeriesPage(CRUD).php"
+                </script>';
+        }else{
+            echo
+                '<script>
+                alert("Edit Series Failed");
+                window.location = "../page/editSeriesPage(CRUD).php"
+                </script>';
+        }
+        
+        }else{
+        echo
+            '<script>
+            window.history.back()
+            </script>';
+    }
+?>
---
+++ process/insertSeriesProcess.php
@@ -0,0 +1,41 @@
+<?php
+    session_start();
+    if(isset($_POST['editSeries'])){
+
+        // untuk mengoneksikan dengan database dengan memanggil file db.php
+        include('../db.php');
+
+        // tampung nilai yang ada di from ke variabel
+        // sesuaikan variabel name yang ada di registerPage.php disetiap input
+
+        $name = $_POST['name'];
+        $genre = $_POST['genre'];
+        $realease = $_POST['realease'];
+        $episode = $_POST['episode'];
+        $season = $_POST['season'];
+        $synopsis = $_POST['synopsis'];
+
+        $query = mysqli_query($con," INSERT INTO series(name, genre, realease, episode, season, synopsis) VALUES('$name', '$genre', '$realease', '$episode', '$season', '$synopsis')") 
+                or die(mysqli_error($con)); 
+
+        if($query){
+            echo
+                '<script>
+                alert("Insert Series Success"); 
+                window.location = "../page/insertSeriesPage(CRUD).php"
+                </script>';
+        }else{
+            echo
+                '<script>
+                alert("Edit Series Failed");
+                window.location = "../page/insertSeriesPage(CRUD).php"
+                </script>';
+        }
+        
+        }else{
+        echo
+            '<script>
+            window.history.back()
+            </script>';
+    }
+?>
